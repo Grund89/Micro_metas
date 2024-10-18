a50547166b39771b6d50c00c0b95ce20bf84fed0
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-18 20:53:40 -0300

    feat: adiciona classe Estudante com curso

--- apresentacao_estudadnte.rb
+++ apresentacao_estudadnte.rb
@@ -10,3 +10,12 @@ class Pessoa
     puts "Olá, meu nome é #{nome} e tenho #{idade} anos."
   end
 end
+
+class Estudante < Pessoa
+  attr_accessor :curso
+
+  def initialize(nome, idade, curso)
+    super(nome, idade) # Chama o construtor da classe pai
+    @curso = curso
+  end
+end

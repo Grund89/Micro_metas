b8fb0391739f5686a0088bd77eb343b1b5d1bfec
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-18 20:57:21 -0300

    feat: define meto apresentar e cria objetos pessoa e estudante

--- apresentacao_estudadnte.rb
+++ apresentacao_estudadnte.rb
@@ -18,4 +18,17 @@ class Estudante < Pessoa
     super(nome, idade) # Chama o construtor da classe pai
     @curso = curso
   end
+
+  def apresentar
+    super # Chama o método apresentar da classe pai
+    puts "Sou estudante do curso de #{curso}."
+  end
+
+  # Criando objetos
+  pessoa1 = Pessoa.new("João", 30)
+  estudante1 = Estudante.new("Maria", 20, "Engenharia")
+
+  # Chamando o método apresentar
+  pessoa.apresentar
+  estudante.apresentar
 end

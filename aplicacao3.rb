aed9bbf644145e8c3802518c25a7d2bd6e46f311
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-19 00:55:50 -0300

    feat: cria classe pessoa

--- aplicacao3.rb
+++ aplicacao3.rb
@@ -2,3 +2,12 @@
 # que herdam e compartilham funcionalidades.
 # Exemplo: um sistema de gerenciamento de funcionários, onde
 # Gerente e Funcionário têm comportamentos distintos.
+
+class Pessoa
+  attr_accessor :nome, :cpf
+
+  def initialize(nome, cpf)
+    @nome = nome
+    @cpf = cpf
+  end
+end

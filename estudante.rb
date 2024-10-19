3f210cf672fba3f6f438dae2ecdf2bcd4b155b51
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-18 21:04:16 -0300

    fix: retira objetos e chamada do metodo de dentro da classe estudante

--- estudante.rb
+++ estudante.rb
@@ -23,12 +23,12 @@ class Estudante < Pessoa
     super # Chama o método apresentar da classe pai
     puts "Sou estudante do curso de #{curso}."
   end
-
-  # Criando objetos
-  pessoa1 = Pessoa.new("João", 30)
-  estudante1 = Estudante.new("Maria", 20, "Engenharia")
-
-  # Chamando o método apresentar
-  pessoa.apresentar
-  estudante.apresentar
 end
+
+# Criando objetos
+pessoa = Pessoa.new("João", 30)
+estudante = Estudante.new("Maria", 20, "Engenharia")
+
+# Chamando o método apresentar
+pessoa.apresentar
+estudante.apresentar

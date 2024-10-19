810a76d4bf41eed2239f60bf39c7f8101ab2ac13
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-19 00:54:47 -0300

    build: first commit

---
+++ aplicacao3.rb
@@ -0,0 +1,4 @@
+# Desenvolver um sistema que simule uma empresa, com classes
+# que herdam e compartilham funcionalidades.
+# Exemplo: um sistema de gerenciamento de funcionários, onde
+# Gerente e Funcionário têm comportamentos distintos.

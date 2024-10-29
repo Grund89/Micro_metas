6816910129be74b1e29601f04a7e42034d660e1c
Douglas Grund <douglasgrund@yahoo.com.br>
2024-10-29 16:01:57 -0300

    feat: add enunciado da tarefa

--- aplicacao3/desafio_semana5.rb
+++ aplicacao3/desafio_semana5.rb
@@ -1,3 +1,8 @@
+# Herança em OOP: criar uma classe que herda de outra.
+# Aprender o conceito de herança em OOP, criando uma classe filha
+# que herda atributos e métodos da classe pai.
+# Exemplo: Estudante herdando de Pessoa.
+
 class Veiculo
   attr_accessor :marca, :modelo, :ano
 
